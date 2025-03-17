cd560d8a0a9590783fbb18258ee218d6a613c405
Shaikh-Je <156939469+Shaikh-Je@users.noreply.github.com>
2025-03-17 20:41:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -137,4 +137,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Auto slide every 5 seconds
   setInterval(() => moveSlide(1), 3000);
+
+
+///////////// Get the modal ////////////////////
+  var modal = document.getElementById("buyNowModal");
+
+  // Get the button that opens the modal
+  var btns = document.querySelectorAll(".buy-now-btn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal 
+  btns.forEach(function(btn) {
+    btn.onclick = function() {
+      modal.style.display = "block";
+    };
+  });
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 });
